60c417dff3e1ca2115e1ff3657e7c7cddef5d9e5
GuilhermePortella <guilhermeportella2@gmail.com>
2025-07-01 23:22:13 -0300

    add initial model classes for OperacaoConta, TipoOperacao, ContaPF, and Usuario

---
+++ src/main/java/br/domain/model/usuarios/ContaPF.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package br.domain.model.usuarios;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class ContaPF {
+
+}
